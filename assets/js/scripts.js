ac2ac1502f7240566646c11b06620b86d440f850
sexfat <bryant6831@gmail.com>
2020-03-23 16:28:30 +0800

    add minicss

--- assets/js/scripts.js
+++
@@ -1,1 +0,0 @@
-console.log('外部連結js ok');
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
 
 
 gulp.task('concatjs' , function() {
-    gulp.src('js/*').pipe(gulp.dest('assets/js'));
+    gulp.src('js/*.js').pipe(gulp.dest('assets/js'));
 })
 
 gulp.task('concatcss' , function() {
-    gulp.src('css/*').pipe(gulp.dest('assets/css'));
+    gulp.src('css/*.css').pipe(gulp.dest('assets/css'));
 })
+
+
+gulp.task('minicss', function () {
+    gulp.src('css/*.css')
+    //壓縮
+    .pipe(cleanCSS({
+        compatibility: 'ie8'
+    }))
+    //完成搬家
+    .pipe(gulp.dest('css/mini/'));
+});
+
