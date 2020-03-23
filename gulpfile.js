df0568a49ad62c3a62047c543fd831951c7aec72
sexfat <bryant6831@gmail.com>
2020-03-23 16:46:20 +0800

    add minicss sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,31 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
+var sass = require('gulp-sass');
 
 
-gulp.task('concatjs' , function() {
+gulp.task('concatjs', function () {
     gulp.src('js/*.js').pipe(gulp.dest('assets/js'));
 })
 
-gulp.task('concatcss' , function() {
+gulp.task('concatcss', function () {
     gulp.src('css/*.css').pipe(gulp.dest('assets/css'));
 })
 
 
-gulp.task('minicss', function () {
-    gulp.src('css/*.css')
-    //壓縮
-    .pipe(cleanCSS({
-        compatibility: 'ie8'
-    }))
-    //完成搬家
-    .pipe(gulp.dest('css/mini/'));
+gulp.task('sass', function () {
+    gulp.src('sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css/'))
 });
+
+gulp.task('minicss',['sass'], function () {
+    gulp.src('css/*.css')
+        //壓縮
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        //完成搬家
+        .pipe(gulp.dest('css/mini/'));
+});
+
 
